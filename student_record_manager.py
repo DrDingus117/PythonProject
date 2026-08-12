672fdd02401094bf296476ac03d751e52d779473
Marcus Pendleton <marcuspendleton100@gmail.com>
2026-08-12 13:50:31 -0700

    Basic menu with two options

--- student_record_manager.py
+++ student_record_manager.py
@@ -1,19 +1,21 @@
 # Student Record Manager based off the Java version
 
-# 1: Student Class begining
+# 1: Student Class beginning
 
 class Student:
-  def __init__(self, student_name, student_id, grades, program, age, quarter):
-    self .student_name = student_name
-    self .student_id = student_id
-    self .grades = grades
-    self .program = program
-    self .age = age
-    self .quarter = quarter
+    def __init__(self, student_name, student_id, grades, program, age, quarter):
+        self.student_name = student_name
+        self.student_id = student_id
+        self.grades = grades
+        self.program = program
+        self.age = age
+        self.quarter = quarter
 
-# 2: adding Student objects
+
+# 2: Adding Student objects
 
 students = []
+
 
 def add_student():
     student_name = input("Enter student name: ")
@@ -26,3 +28,27 @@ def add_student():
     student = Student(student_name, student_id, grades, program, age, quarter)
 
     students.append(student)
+
+
+# 3: Menu
+
+def menu():
+    while True:
+        print("\nStudent Record Manager")
+        print("1. Add Student")
+        print("2. Exit")
+
+        choice = input("Enter the choice you want to do: ")
+
+        if choice == "1":
+            add_student()
+
+        elif choice == "2":
+            print("See Ya!")
+            break
+
+        else:
+            print("Can't do that. Please try again.")
+
+
+menu()
