89330191e953d9d56bc6f682c4e28314f52be112
Marcus Pendleton <marcuspendleton100@gmail.com>
2026-08-12 13:43:54 -0700

    Objects

--- student_record_manager.py
+++ student_record_manager.py
@@ -10,3 +10,19 @@ class Student:
     self .program = program
     self .age = age
     self .quarter = quarter
+
+# 2: adding Student objects
+
+students = []
+
+def add_student():
+    student_name = input("Enter student name: ")
+    student_id = input("Enter student ID: ")
+    grades = input("Enter student grades (comma-separated): ")
+    program = input("Enter student program: ")
+    age = int(input("Enter student age: "))
+    quarter = input("Enter student quarter: ")
+
+    student = Student(student_name, student_id, grades, program, age, quarter)
+
+    students.append(student)
