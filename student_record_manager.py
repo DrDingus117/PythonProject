49c7f6cabb7d29481b96d67aa460d8d3d55d6b44
Marcus Pendleton <marcuspendleton100@gmail.com>
2026-08-12 13:35:29 -0700

    added quarter

--- student_record_manager.py
+++ student_record_manager.py
@@ -3,9 +3,10 @@
 # 1: Student Class begining
 
 class Student:
-  def __init__(self, student_name, student_id, grades, program, age):
+  def __init__(self, student_name, student_id, grades, program, age, quarter):
     self .student_name = student_name
     self .student_id = student_id
     self .grades = grades
     self .program = program
     self .age = age
+    self .quarter = quarter
