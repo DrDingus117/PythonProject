9bfb9c786759253c03f391c9913a23aea132f261
Marcus Pendleton <marcuspendleton100@gmail.com>
2026-08-18 12:51:35 -0700

    Finished project

--- CSV/csv_to_insights.py
+++ CSV/csv_to_insights.py
@@ -23,6 +23,10 @@ results = {
     "mean": statistics.mean(values),
     "maximum": max(values)
 }
+
 # 4: Write to JSON
+with open("results.json", "w") as file:
+    json.dump(results, file)
 
 # 5: Verify in Terminal
+print(results)
