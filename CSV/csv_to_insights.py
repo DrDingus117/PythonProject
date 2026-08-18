e05baf5cb7540df3d0616048c4472b830118dd3d
Marcus Pendleton <marcuspendleton100@gmail.com>
2026-08-18 12:20:57 -0700

    step 3 done

--- CSV/csv_to_insights.py
+++ CSV/csv_to_insights.py
@@ -18,7 +18,11 @@ except FileNotFoundError:
     print("CSV file not found.")
 
 # 3: Compute Statistics
-
+results = {
+    "minimum": min(values),
+    "mean": statistics.mean(values),
+    "maximum": max(values)
+}
 # 4: Write to JSON
 
 # 5: Verify in Terminal
