a948f318456f4c4b4b051cb85ac528b776c7bb0b
Marcus Pendleton <marcuspendleton100@gmail.com>
2026-08-18 12:06:08 -0700

    Realized i was doing everything in 1 and spaced  over 2

--- CSV/csv_to_insights.py
+++ CSV/csv_to_insights.py
@@ -3,15 +3,19 @@ import csv
 import statistics
 import json
 
-with open("sample-superstore.csv", "r", encoding="utf-8") as file:
-    reader = csv.DictReader(file)
-
-    values = []
-    
-    for row in reader:
-        values.append(float(row["Sales"]))
-        print(row)
 # 2: Handle Errors
+try:
+    with open("sample-superstore.csv", "r", encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+
+        values = []
+
+        for row in reader:
+            values.append(float(row["Sales"]))
+            print(row)
+
+except FileNotFoundError:
+    print("CSV file not found.")
 
 # 3: Compute Statistics
 
